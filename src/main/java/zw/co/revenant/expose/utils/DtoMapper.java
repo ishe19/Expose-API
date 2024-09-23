9f74aa203ad3b5703d7ed06c6a7fb8b37adc08c1
Ishe <38261770+ishe19@users.noreply.github.com>
2024-09-23 21:53:43 +0200

    Update DtoMapper.java

--- src/main/java/zw/co/revenant/expose/utils/DtoMapper.java
+++ src/main/java/zw/co/revenant/expose/utils/DtoMapper.java
@@ -2,7 +2,16 @@ package zw.co.revenant.expose.utils;
 
 import zw.co.revenant.expose.features.articles.models.dto.ArticleDto;
 import zw.co.revenant.expose.features.articles.models.entities.Article;
+import zw.co.revenant.expose.features.leaks.models.dto.LeakDto;
+import zw.co.revenant.expose.features.leaks.models.entities.Leak;
+import zw.co.revenant.expose.features.learning.models.dto.LearningArticleDto;
+import zw.co.revenant.expose.features.learning.models.entities.LearningArticle;
 
 public interface DtoMapper {
     ArticleDto mapToArticleDto(Article article);
+
+    LeakDto mapToLeakDto(Leak leak);
+
+    LearningArticleDto mapToLearningArticleDto(LearningArticle learningArticle);
+
 }
