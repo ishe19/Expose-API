f70a325a7f7c2acbf08db9af98aa74e960bbc5db
Ishe <38261770+ishe19@users.noreply.github.com>
2024-09-23 21:53:41 +0200

    Update DtoMapperImpl.java

--- src/main/java/zw/co/revenant/expose/utils/DtoMapperImpl.java
+++ src/main/java/zw/co/revenant/expose/utils/DtoMapperImpl.java
@@ -2,6 +2,10 @@ package zw.co.revenant.expose.utils;
 
 import zw.co.revenant.expose.features.articles.models.dto.ArticleDto;
 import zw.co.revenant.expose.features.articles.models.entities.Article;
+import zw.co.revenant.expose.features.leaks.models.dto.LeakDto;
+import zw.co.revenant.expose.features.leaks.models.entities.Leak;
+import zw.co.revenant.expose.features.learning.models.dto.LearningArticleDto;
+import zw.co.revenant.expose.features.learning.models.entities.LearningArticle;
 
 public class DtoMapperImpl implements DtoMapper {
     @Override
@@ -13,6 +17,27 @@ public class DtoMapperImpl implements DtoMapper {
                 article.getBody(),
                 String.format("%s %s", article.getJournalist().getFirstName(), article.getJournalist().getLastName()),
                 article.getCreatedOn()
+        );
+    }
+
+    @Override
+    public LeakDto mapToLeakDto(Leak leak) {
+        return new LeakDto(
+                leak.getTitle(),
+                leak.getLeakCode(),
+                leak.getDescription(),
+                leak.getCreatedOn()
+        );
+    }
+
+    @Override
+    public LearningArticleDto mapToLearningArticleDto(LearningArticle learningArticle) {
+        return new LearningArticleDto(
+                learningArticle.getArticleCode(),
+                learningArticle.getTitle(),
+                learningArticle.getBody(),
+                String.format("%s %s", learningArticle.getJournalist().getFirstName(), learningArticle.getJournalist().getLastName()),
+                learningArticle.getCreatedOn()
         );
     }
 }
