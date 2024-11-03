e74d962b5347267cd4f75fa6bf7f7a136738e072
Isheanesu Gwangwanyu <38261770+ishe19@users.noreply.github.com>
2024-11-03 14:59:10 +0200

    Update LearningService.java

--- src/main/java/zw/co/revenant/expose/features/learning/services/interfaces/LearningService.java
+++ src/main/java/zw/co/revenant/expose/features/learning/services/interfaces/LearningService.java
@@ -1,8 +1,11 @@
 package zw.co.revenant.expose.features.learning.services.interfaces;
 
 import org.springframework.http.ResponseEntity;
+import zw.co.revenant.expose.features.articles.models.requests.PostArticleRequest;
 import zw.co.revenant.expose.utils.models.ExposeResponse;
 
 public interface LearningService {
     ResponseEntity<ExposeResponse> getArticles();
+
+    ResponseEntity<ExposeResponse> postLearningArticle(PostArticleRequest request);
 }
