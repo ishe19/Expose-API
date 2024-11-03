63bae1641be41de80bab8896a26c6a9921c26904
Isheanesu Gwangwanyu <38261770+ishe19@users.noreply.github.com>
2024-11-03 14:59:00 +0200

    Update ExposeApiApplication.java

--- src/main/java/zw/co/revenant/expose/ExposeApiApplication.java
+++ src/main/java/zw/co/revenant/expose/ExposeApiApplication.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "EXPOSE API", version = "0.1", description = "REST API for EXPOSE "))
 public class ExposeApiApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ExposeApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ExposeApiApplication.class, args);
+    }
 }
